5dac80c3978ea5bb180abbbe02b53803c0900f28
definitelynotdragon <itielseriol@gmail.com>
2019-12-02 22:09:36 -0600

    Ejercicio 1

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+var PesoMexicano = 1
+var DolarAmericano = 19 PesoMexicano
+var Euro = 22 PesoMexicano
+var PesoCubanoNacional = 0.05 PesoMexicano
+var PesoCubanoIntercambiable = 26500 PesoCubanoNacional
