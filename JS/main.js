a4a214376493a070173db75d952879396f287bae
definitelynotdragon <itielseriol@gmail.com>
2019-12-03 19:18:42 -0600

    Ejercicio 2

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,12 @@ var DolarAmericano = 19 PesoMexicano
 var Euro = 22 PesoMexicano
 var PesoCubanoNacional = 0.05 PesoMexicano
 var PesoCubanoIntercambiable = 26500 PesoCubanoNacional
+
+'Ejercicio Dos'
+
+var Year = 365 days 
+var Month = 30 days
+var Day = 1
+var Hour = 60
+var Minutes = 60/6
+var Second = (Minutes/Hour)
